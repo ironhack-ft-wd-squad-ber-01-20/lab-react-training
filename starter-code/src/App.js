b47212a7e2b799f84863f2e88088c2ea482783af
carlosziegler <carlos.ziegler@gmail.com>
2020-07-14 11:56:47 +0200

    component IdCard

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import IdCard from './components/IdCard'
+import Profiles from './data/berlin.json'
 
 class App extends Component {
+
   render() {
+    console.log(Profiles[0])
     return (
       <div className="App">
         <h1>IdCard</h1>
         {/* TODO: Use the IdCard component */}
+        {Profiles && Profiles.map((profile, index) =>
+
+          <IdCard key={index}
+            lastName={profile.lastName}
+            firstName={profile.firstName}
+            gender={profile.gender}
+            height={profile.height}
+            birth={new Date("1988-05-11")}
+            picture={profile.img}
+          />)}
+
+
 
         <h1>Greetings</h1>
         {/* TODO: Use the Greetings component */}
