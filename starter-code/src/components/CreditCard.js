43929742f0754553353a8850aa62f09f0e58f1d0
SiEr0212 <simonertel@gmx.de>
2020-07-14 19:16:38 +0200

    Credit Card done

--- starter-code/src/components/CreditCard.js
+++ starter-code/src/components/CreditCard.js
@@ -1,59 +1,66 @@
-import React from 'react';
-import "./CreditCard.css"
+import React from "react";
+import "./CreditCard.css";
 
 export default function CreditCard(props) {
+  const {
+    type,
+    number,
+    expirationMonth,
+    expirationYear,
+    bank,
+    owner,
+    bgColor,
+    color,
+  } = props;
 
-    const {type, number, expirationMonth, expirationYear, bank, owner, bgColor, color } = props;
-    
-    let card = './img/master-card.svg';
+  let card = "./img/master-card.svg";
 
-     if (type === 'Visa') {
-         card = './img/visa.png'}
+  if (type === "Visa") {
+    card = "./img/visa.png";
+  }
 
-    const style = {
-        width: "400px",
-        color: color,
-        backgroundColor: bgColor,
-        display: "flex",
-        flexDirection: "column",
-        borderRadius: "5%",
-        marginLeft: "10px",
-    };
-    
-    
-    const imgStyle = {
-        width: '30%'
-    };
+  const style = {
+    width: "400px",
+    color: color,
+    backgroundColor: bgColor,
+    display: "flex",
+    flexDirection: "column",
+    borderRadius: "5%",
+    marginLeft: "10px",
+  };
 
-    
-    function maskify(cc) {
-        const masked = cc.slice(-4);
-        let countNum = "";
-        for (let i = (cc.length) - 4; i > 0; i--) {
-            countNum += "#"
-        }
-        return countNum + masked
+  const imgStyle = {
+    width: "30%",
+  };
+
+  function maskify(cc) {
+    const masked = cc.slice(-4);
+    let countNum = "";
+    for (let i = cc.length - 4; i > 0; i--) {
+      countNum += "#";
     }
+    return countNum + masked;
+  }
 
-    return (
-        <div style={style} >
-            <div>
-                <img src={card} alt="stupid plastic cards" style={imgStyle} />
-            </div>
+  return (
+    <div style={style}>
+      <div>
+        <img src={card} alt="stupid plastic cards" style={imgStyle} />
+      </div>
 
-            <div>
-                <p>{maskify(number)}</p>
-            </div>
+      <div>
+        <p>{maskify(number)}</p>
+      </div>
 
-            <div className= "flexCardsBox">
-                <p>{expirationMonth}</p>
-                <p>{expirationYear}</p>
-                <p>{bank}</p>
-            </div>
+      <div className="flexCardsBox">
+        <p>{expirationMonth}</p>
+        <p>{expirationYear}</p>
+        <p>{bank}</p>
+      </div>
 
-            <div>
-                <p>{owner}</p>
-            </div>
-        </div>
-    )
+      <div>
+        <p>{owner}</p>
+      </div>
+    </div>
+  );
 }
