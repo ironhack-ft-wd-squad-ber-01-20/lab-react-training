d26db7d5f53e1b92e2eece5972d70f05e8ee2aa0
Leonardo Costa <leo.costa.programming@gmail.com>
2020-07-14 19:49:18 +0200

    DriverCard + LikeButton Components

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -5,6 +5,8 @@ import Random from "./Components/Random";
 import BoxColor from "./Components/BoxColor";
 import CreditCard from "./Components/CreditCard";
 import Rating from "./Components/Rating";
+import DriverCard from "./Components/DriverCard";
+import LikeButton from "./Components/LikeButton";
 
 const App = () => {
   return (
@@ -89,6 +91,30 @@ const App = () => {
       <Rating>4</Rating>
       <Rating>5</Rating>
 
+      <h1>DriverCard</h1>
+      <DriverCard 
+      name="Travis Kalanick"
+      rating={4.2}
+      img="https://si.wsj.net/public/resources/images/BN-TY647_37gql_OR_20170621052140.jpg?width=620&height=428"
+      car={{
+        model: "Toyota Corolla Altis",
+        licensePlate: "CO42DE"
+      }} />
+      <DriverCard 
+        name="Dara Khosrowshahi"
+        rating={4.9}
+        img="https://ubernewsroomapi.10upcdn.com/wp-content/uploads/2017/09/Dara_ELT_Newsroom_1000px.jpg"
+        car={{
+          model: "Audi A3",
+          licensePlate: "BE33ER"
+      }} />
+
+
+      <h1>LikeButton</h1>
+      <div className="like-btn">
+        <LikeButton />
+        <LikeButton />    
+      </div>
     </div>
   );
 }
