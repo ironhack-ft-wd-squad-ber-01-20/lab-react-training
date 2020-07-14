9fd77e5d326825133388ea2759b53981b007fd93
SiEr0212 <simonertel@gmx.de>
2020-07-14 18:11:11 +0200

    BackgroundColor done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,59 +1,56 @@
-import React, { Component } from 'react';
-import IdCard from './components/IdCard.js';
-import Greetings from './components/Greetings.js';
-import Random from './components/Random';
+import React, { Component } from "react";
+import IdCard from "./components/IdCard.js";
+import Greetings from "./components/Greetings.js";
+import Random from "./components/Random.js";
+import BoxColor from "./components/BoxColor.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>IdCard</h1>
-        <div className = "idCards"><IdCard
-  lastName='Doe'
-  firstName='John'
-  gender='male'
-  height={178}
-  birth={new Date("1992-07-14")}
-  picture="https://randomuser.me/api/portraits/men/44.jpg"
-/>
-</div> 
-<div className = "idCards">
-<IdCard
-  lastName='Delores '
-  firstName='Obrien'
-  gender='female'
-  height={172}
-  birth={new Date("1988-05-11")}
-  picture="https://randomuser.me/api/portraits/women/44.jpg"
-/>
-</div>
+        <div className="idCards">
+          <IdCard
+            lastName="Doe"
+            firstName="John"
+            gender="male"
+            height={178}
+            birth={new Date("1992-07-14")}
+            picture="https://randomuser.me/api/portraits/men/44.jpg"
+          />
+        </div>
+        <div className="idCards">
+          <IdCard
+            lastName="Delores "
+            firstName="Obrien"
+            gender="female"
+            height={172}
+            birth={new Date("1988-05-11")}
+            picture="https://randomuser.me/api/portraits/women/44.jpg"
+          />
+        </div>
 
         <h1>Greetings</h1>
-        <div className = "greetings">
-        <Greetings lang="de">Ludwig</Greetings>
-        </div>
-        <div className = "greetings">
-        <Greetings lang="fr">François</Greetings>
-        </div> 
+        
+          <Greetings lang="de">Ludwig</Greetings>
+          <Greetings lang="fr">François</Greetings>
+        
 
         <h1>Random</h1>
-        <div clasName ="random">
-        <Random min={1} max={6}/>
-        <Random min={1} max={100}/>
+        <div clasName="random">
+          <Random min={1} max={6} />
+          <Random min={1} max={100} />
         </div>
 
+        <h1>BoxColor</h1>
+        
+        <BoxColor r={255} g={0} b={0} />
+        <BoxColor r={128} g={255} b={0} />
+        
 
 
 
-
-
-
-
-
-        </div>
-
-            
-            
+      </div>
     );
   }
 }
