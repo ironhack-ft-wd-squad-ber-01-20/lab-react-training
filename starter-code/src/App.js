91d79899bf9c21075ea6f6fd3fd66dc5c6678cb3
SiEr0212 <simonertel@gmx.de>
2020-07-14 19:16:05 +0200

    Credit Card done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -3,6 +3,7 @@ import IdCard from "./components/IdCard.js";
 import Greetings from "./components/Greetings.js";
 import Random from "./components/Random.js";
 import BoxColor from "./components/BoxColor.js";
+import CreditCard from "./components/CreditCard";
 
 class App extends Component {
   render() {
@@ -31,10 +32,9 @@ class App extends Component {
         </div>
 
         <h1>Greetings</h1>
-        
-          <Greetings lang="de">Ludwig</Greetings>
-          <Greetings lang="fr">François</Greetings>
-        
+
+        <Greetings lang="de">Ludwig</Greetings>
+        <Greetings lang="fr">François</Greetings>
 
         <h1>Random</h1>
         <div clasName="random">
@@ -43,13 +43,43 @@ class App extends Component {
         </div>
 
         <h1>BoxColor</h1>
-        
+
         <BoxColor r={255} g={0} b={0} />
         <BoxColor r={128} g={255} b={0} />
-        
 
-
-
+        <h1>Credit Card</h1>
+        <div className = "creditCard">
+        <CreditCard
+          type="Visa"
+          number="0123456789018845"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="BNP"
+          owner="Maxence Bouret"
+          bgColor="#11aa99"
+          color="white"
+        />
+        <CreditCard
+          type="Master Card"
+          number="0123456789010995"
+          expirationMonth={3}
+          expirationYear={2021}
+          bank="N26"
+          owner="Maxence Bouret"
+          bgColor="#eeeeee"
+          color="#222222"
+        />
+        <CreditCard
+          type="Visa"
+          number="0123456789016984"
+          expirationMonth={12}
+          expirationYear={2019}
+          bank="Name of the Bank"
+          owner="Firstname Lastname"
+          bgColor="#ddbb55"
+          color="white"
+        />
+        </div>
       </div>
     );
   }
