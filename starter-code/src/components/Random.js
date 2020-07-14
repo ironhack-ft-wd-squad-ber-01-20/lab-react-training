98da19873ffccb244b920cfacf6bc5754b411176
vmayrock <verena.mayrock@gmail.com>
2020-07-14 12:07:07 +0200

    random number done

--- starter-code/src/components/Random.js
+++ starter-code/src/components/Random.js
@@ -1,9 +1,10 @@
 import React from 'react'; 
 
 function Random(props) {
+    let random=Math.floor(Math.random() * (props.max - props.min) + props.min); 
     return (
         <div className="container">
-        <p>Random value between {props.min} and {props.max}.</p> 
+        <p>Random value between {props.min} and {props.max} => {random}</p> 
         </div>
     ); 
 };
