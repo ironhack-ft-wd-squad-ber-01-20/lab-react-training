5971916e9cc67a01477829c658440cf57c856188
Ivan Mickovski <ivan_mickovski@hotmail.com>
2020-07-14 16:59:20 +0200

    Dice Component

---
+++ starter-code/src/components/Dice.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+
+class Dice extends Component {
+    state = {
+        img: './img/dice3.png'
+    }
+
+    clickHandler = () => {
+        this.setState({
+            img: './img/dice-empty.png'
+        });
+        let randomNum = Math.floor(Math.random() * 6);
+        let img = `./img/dice${randomNum}.png`;
+
+        setTimeout(() => {
+            this.setState({
+                img: img
+            })
+        }, 1000);
+    }
+    render() {
+        let dice = './img/dice3.png';
+        let imgStyle = {
+            marginLeft: '50px',
+            width: '200px'
+        }
+        return(
+            <div>
+                <img onClick={this.clickHandler} style={imgStyle} src={this.state.img} />
+            </div>
+        )
+    }
+}
+
+export default Dice
