e2888d960885e7388b832a5fcc6fcb00ced35e99
SiEr0212 <simonertel@gmx.de>
2020-07-14 12:18:17 +0200

    Iteration 1 done

---
+++ starter-code/src/components/IdCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const IdCard = (props) => {
+    
+      return(
+        <div>
+        <div className="idcard">
+            <p><strong>First name:</strong> {props.firstName}</p>
+            <p><strong>Last name:</strong> {props.lastName}</p>
+            <p><strong>Gender:</strong> {props.gender}</p>
+            <p><strong>Height:</strong> {props.height}</p>
+            <p><strong>Birth:</strong> {props.birth.toDateString()}</p>
+            <img src={props.picture} alt={props.firstName} />
+        </div>
+        </div>
+        
+        
+    )
+ 
+}
+
+export default IdCard;
